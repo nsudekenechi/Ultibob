758c70c19c119d4c5e735309e22eb07ea4c533db
nsudekenechi <nsudekenechi2@gmail.com>
2022-09-24 03:02:18 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let nav = document.querySelector("header nav ");
+window.onscroll = function(e){
+ if(window.scrollY>=20){
+    nav.classList.add("sticky")
+ }else{
+    nav.classList.remove("sticky")
+ }
+ 
+}
+const loader = document.querySelector("#loader ");
+let text = "Ultibob.".split("");
+window.onload = function(){
+   
+   setInterval(()=>{
+   
+      if(text.length>0){
+          loader.querySelector("h1").innerHTML+=text.shift();
+         
+      }else{
+       loader.style.display="none"
+       document.body.classList.remove("hideScroll")
+      }
+    },500)
+}
