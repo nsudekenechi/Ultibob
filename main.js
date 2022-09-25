18ae584addbc643d9e45afe8c6530f951662f701
nsudekenechi <nsudekenechi2@gmail.com>
2022-09-25 01:23:24 +0100

    Added cloud services page

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let nav = document.querySelector("header nav ");
 window.onscroll = function(e){
- if(window.scrollY>=20){
+ if(window.scrollY>0){
     nav.classList.add("sticky")
  }else{
     nav.classList.remove("sticky")
